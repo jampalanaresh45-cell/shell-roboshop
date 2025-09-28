d3fdff32ded2ce23c0ec228b65648187c80f243f
jampalanaresh45-cell <jampalanaresh45@gmail.com>
2025-09-28 20:16:26 +0530

    Roboshop

---
+++ roboshop.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-0b27bdc41b21e4e6d" #Replace with sg id of your ec2 instance
+
+for instance in $@
+do
+    INSTANCE_ID=$(awsec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0b27bdc41b21e4e6d --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+
+###Get Private IP address of the instance
+    if [ $instance == "Frontend" ]; then
+        IP=$(aws ec2 describe-instances --instance-ids i-01447222617677034 --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+
+    else
+        IP=$(aws ec2 describe-instances --instance-ids i-01447222617677034 --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+
+    fi
+
+        echo "$instance: $IP"
+
+done
