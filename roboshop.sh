bef71c85de13ec01b77c7aafea0f56e07cc34587
Naresh Jampala <jampalanaresh45@gmail.com>
2026-04-14 17:59:16 +0530

    shell-roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -6,7 +6,7 @@ ZONE_ID="Z01475058I1W1ZI7MBLH" #Replace with your hosted zone id
 DOMAIN_NAME="daws86s.store" #Replace with your domain name
 for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
 ###Get Private IP address of the instance
     if [ $instance != "Frontend" ]; then
